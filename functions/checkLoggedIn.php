3e8bb54f7e65a07833b69b5ff19966e00fd79daa
Menno van den Ende <mentosmenno2@hotmail.com>
2017-04-01 13:11:39 +0200

    fix redirect login

--- functions/checkLoggedIn.php
+++ functions/checkLoggedIn.php
@@ -1,6 +1,6 @@
 <?php
 	if (!isset($_SESSION["userID"]) || $_SESSION["userID"] < 1) {
-		$redirectedFrom = basename($_SERVER['PHP_SELF'], '.php');
+		$redirectedFrom = basename($_SERVER['PHP_SELF']);
 		header("location: login?redirectedFrom=" . $redirectedFrom);
 		die();
 	}
